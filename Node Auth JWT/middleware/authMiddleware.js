6a4f8fcc92a4aa8d7f8b3f02518991dbcca8eb17
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-12-09 16:56:29 -0600

    Finished-Node Auth #17

--- Node Auth JWT/middleware/authMiddleware.js
+++ Node Auth JWT/middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
-
     const token = req.cookies.jwt;
 
     // check json web token exist & verified
@@ -21,4 +20,30 @@ const requireAuth = (req, res, next) => {
         res.redirect("/login");
     }
 }
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "net ninja secret", async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            }
+            else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    }
+    else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
